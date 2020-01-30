9eafa0781a062174f98d7c91f6859827ffc641f2
Ray Zane <raymondzane@gmail.com>
2020-01-30 17:01:15 -0500

    Start building out a CLI

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/mattn/go-isatty v0.0.12
 	github.com/pkg/errors v0.9.1
 	github.com/rakyll/statik v0.1.6
+	github.com/urfave/cli/v2 v2.1.1
 )
--- main.go
+++ main.go
@@ -2,32 +2,73 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"os"
 
-	"github.com/rakyll/statik/fs"
-	_ "github.com/rzane/binny/statik"
-	binny "github.com/rzane/binny/pkg"
+	"github.com/urfave/cli/v2"
 )
 
-func main() {
-	config := binny.Config{
-		Image:   "binny",
-		Load:    true,
-		Workdir: "/workdir",
-		Args:    os.Args[1:],
-		Open: func() (http.File, error) {
-			statik, err := fs.New()
-			if err != nil {
-				return nil, err
-			}
+type options struct {
+	Image   string
+	Build   string
+	Embed   bool
+	Workdir string
+	Env     []string
+	Volumes []string
+}
 
-			return statik.Open("/image.tar.gz")
+func main() {
+	app := &cli.App{
+		Name:   "binny",
+		Usage:  "create an executable from a docker image",
+		Action: generate,
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Required: true,
+				Name:     "image",
+				Usage:    "name of your docker image",
+			},
+			&cli.BoolFlag{
+				Name:  "embed",
+				Usage: "embed a docker image in the binary",
+			},
+			&cli.StringFlag{
+				Name:  "build",
+				Usage: "build a docker image upon install",
+			},
+			&cli.StringFlag{
+				Name:    "workdir",
+				Aliases: []string{"w"},
+				Usage:   "mount the user's current directory in the image",
+			},
+			&cli.StringSliceFlag{
+				Name:    "env",
+				Aliases: []string{"e"},
+				Usage:   "whitelist environment variables",
+			},
+			&cli.StringSliceFlag{
+				Name:    "volume",
+				Aliases: []string{"v"},
+				Usage:   "bind mount a volume",
+			},
 		},
 	}
 
-	if err := binny.Shim(config); err != nil {
-		fmt.Fprintf(os.Stderr, "error: %v\n", err)
-		os.Exit(1)
+	app.Run(os.Args)
+}
+
+func generate(c *cli.Context) error {
+	opts := parseOptions(c)
+	fmt.Printf("%+v\n", opts)
+	return nil
+}
+
+func parseOptions(c *cli.Context) options {
+	return options{
+		Image:   c.String("image"),
+		Build:   c.String("build"),
+		Embed:   c.Bool("embed"),
+		Workdir: c.String("workdir"),
+		Env:     c.StringSlice("env"),
+		Volumes: c.StringSlice("volume"),
 	}
 }
